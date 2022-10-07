b39af49b4d464f3d7922604b028451c0f26648af
mashakun <shakunica@gmail.com>
2022-10-07 14:07:01 +0300

    script

--- parrallel_matrix_multiplocation/Header.h
+++ parrallel_matrix_multiplocation/Header.h
@@ -3,5 +3,6 @@
 int* canon(int* A, int* B, int n);
 int* canon_external(int* A, int* B, int n);
 int* canon_internal(int* A, int* B, int n);
+int* blocked(int* A, int* B, int n, int r);
 int* blocked_external(int* A, int* B, int n, int r);
 int* blocked_internal(int* A, int* B, int n, int r);
--- parrallel_matrix_multiplocation/parrallel_matrix_multiplocation.cpp
+++ parrallel_matrix_multiplocation/parrallel_matrix_multiplocation.cpp
@@ -1,29 +1,65 @@
 #include "Header.h"
 #include "tests.h"
-const int n = 1000;
-const int r = 1000;
 
-int main() {
-	runAllTests();
-	/*int* A = new int[n * n];
-	int* B = new int[n * n];
+#include <vector>;
+#include <cstdlib>;
+#include <iostream>
+//const int n = 1000;
+//const int r = 1000;
+
+int* fillMatrix(const int n) {
+	int* M = new int[n * n];
+	const int min = -100;
+	const int max = 100;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			A[i * n + j] = 1;
-			B[i * n + j] = 1;
+			M[i * n + j] = min + std::rand() % (max + 1 - min);
 		}
 	}
 
-	int* C1 = canon(A, B, n);
-	int* C2 = canon_external(A, B, n);
-	int* C3 = canon_internal(A, B, n);*/
-	/*for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			std::cout << C1[i * n + j] << " ";
-		}
-		std::cout << '\n';
-	}*///;'';';';';/////////
+	return M;
+}
+
+int main() {
+	//runAllTests();
+	const int n[10] = { 100, 250, 500, 1000, 1024, 1500, 1750, 2000, 2048, 2250};
+	//const int r[10] = {2, 5, 10, 16, 20, 32, 50, 100, 200, 500};
+
+	std::vector<std::vector<int>> r(10);
+	r[0] = { 2, 5, 10, 20, 50 };
+	r[1] = { 2, 5, 10, 50 };
+	r[2] = { 2, 5, 10, 20, 50, 100, 250 };
+	r[3] = { 2, 5, 10, 20, 50, 100, 250 };
+	r[4] = { 2, 32, 64, 128 };
+	r[5] = { 2, 5, 10, 20, 50, 100, 250, 500 };
+	r[6] = { 2, 5, 10, 50, 250 };
+	r[7] = { 2, 5, 10, 20, 50, 100, 250, 500 };
+	r[8] = { 2, 32, 64, 128, 512 };
+	r[9] = { 2, 5, 10, 50, 250 };
+
+
+	for (int i = 0; i < 10; i++) {
+		int* A = fillMatrix(n[i]);
+		int* B = fillMatrix(n[i]);
+
+		std::cout << "=============== n = " << n[i] << " =================\n";
+		std::cout << "--------------- canon ---------------\n";
+		int* C1 = canon(A, B, n[i]);
+		delete[] C1;
+		std::cout << "--------------- canon_internal ---------------\n";
+		int* C2 = canon_internal(A, B, n[i]);
+		delete[] C2;
+		std::cout << "--------------- canon_external ---------------\n";
+		int* C3 = canon_external(A, B, n[i]);
+		delete[] C3;
+		std::cout << '\n\n';
+
+
+		delete[] A;
+		delete[] B;
+	}
+
 
 	return 0;
 }
